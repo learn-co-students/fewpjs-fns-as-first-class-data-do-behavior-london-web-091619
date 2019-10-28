942f9cd496192923f8ceff854726445f2822db85
Luke Prendergast <luke.prendergast94@gmail.com>
2019-10-28 13:24:12 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(timeStr){
+  const hour = parseInt(timeStr)
+  if (hour < 12){
+    return ("Good Morning")
+  }
+    else if(hour >12 && hour < 17){
+    return ("Good Afternoon")}
+    else if (hour > 17){
+    return ("Good Evening")}
+}
+
+function displayMessage(message){
+  document.getElementById("greeting").innerText = message;
+}
